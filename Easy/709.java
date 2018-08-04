3f5295e3a5a21f7e5105019e7345ddf171ec2897
Songren Zhao <38932090+songrenzhao@users.noreply.github.com>
2018-08-04 10:26:45 -0400

    Create 709.java

---
+++ Easy/709.java
@@ -0,0 +1,23 @@
+/*
+709. To Lower Case
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+Example 2:
+
+Input: "here"
+Output: "here"
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely"
+*/
+
+//Solution in Java
+class Solution {
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
