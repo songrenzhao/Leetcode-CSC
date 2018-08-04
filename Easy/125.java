73517bbb98c358390369ac84598f1a6f8c398b1d
Songren Zhao <38932090+songrenzhao@users.noreply.github.com>
2018-08-04 10:41:18 -0400

    Create 125.java

---
+++ Easy/125.java
@@ -0,0 +1,24 @@
+/*
+125. Valid Palindrome
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+*/
+
+//Solution in Java
+class Solution {
+    public boolean isPalindrome(String s) {
+        String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String rev = new StringBuffer(actual).reverse().toString();
+        return actual.equals(rev);
+    }
+}
