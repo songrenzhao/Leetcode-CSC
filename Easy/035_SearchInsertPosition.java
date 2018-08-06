de7727ecc5bdaf0e75389b58515886112e4c9843
Songren Zhao <38932090+songrenzhao@users.noreply.github.com>
2018-08-05 22:59:33 -0400

    Update 035_SearchInsertPosition.java

--- Easy/035_SearchInsertPosition.java
+++ Easy/035_SearchInsertPosition.java
@@ -13,6 +13,7 @@ class Solution {
         while(left <= right)
         {
             int mid = (right + left) / 2;
+            //int mid = left + (right - left) / 2;
             if(nums[mid] == target)
                 return mid;
             else if(target < nums[mid])
